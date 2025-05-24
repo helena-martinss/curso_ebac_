21b8d5221e5a8f229d96425d22983bd46be2cf36
Helena Martins <helena257424@gmail.com>
2025-05-24 16:49:57 -0300

    exercicio finalizado

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-function Pessoa(nome, idade) {
-    this.nome = nome;
-    this.idade = idade;
-    this.falar = function() {
-        return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`
+class Aluno {
+    constructor(nome, nota) {
+        this.nome = nome;
+        this.nota = nota;
     }
 }
 
-function Homem(nome, idade) {
-    Pessoa.call(this, nome, idade);
+const aluno1 = new Aluno('Helena', '9')
+const aluno2 = new Aluno('Maria', '5')
+const aluno3 = new Aluno('Júlia', '1')
+const aluno4 = new Aluno('Pedro', '7') 
+
+const listaDeAlunos = [aluno1, aluno2, aluno3, aluno4]
+
+function foiAprovado() {
+    for (let i = 0; i < listaDeAlunos.length; i++) {
+        if (listaDeAlunos[i].nota > 6) {
+            console.log(`O aluno(a) ${listaDeAlunos[i].nome} foi aprovado(a) com nota ${listaDeAlunos[i].nota}`)
+        }
+    }
 }
 
-function Mulher(nome, idade) {
-    Pessoa.call(this, nome, idade);
-}
-
-const maria = new Mulher("Maria", 18);
-const pedro = new Homem("Pedro" , 20);
-const helena = new Mulher("Helena" , 22);
-
-console.log(maria.falar());
-console.log(pedro.falar());
-console.log(helena.falar());
+foiAprovado()
