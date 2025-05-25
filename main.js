895f1d03ea2349d2f302658df01f2453b9eb55a2
Helena Martins <helena257424@gmail.com>
2025-05-25 12:15:34 -0300

    adicionando o método filter

--- main.js
+++ main.js
@@ -5,19 +5,17 @@ class Aluno {
     }
 }
 
-const aluno1 = new Aluno('Helena', '9')
-const aluno2 = new Aluno('Maria', '5')
-const aluno3 = new Aluno('Júlia', '1')
-const aluno4 = new Aluno('Pedro', '7') 
+const aluno1 = new Aluno('Helena', 9)
+const aluno2 = new Aluno('Maria', 5)
+const aluno3 = new Aluno('Júlia', 1)
+const aluno4 = new Aluno('Pedro', 7) 
 
 const listaDeAlunos = [aluno1, aluno2, aluno3, aluno4]
 
 function foiAprovado() {
-    for (let i = 0; i < listaDeAlunos.length; i++) {
-        if (listaDeAlunos[i].nota > 6) {
-            console.log(`O aluno(a) ${listaDeAlunos[i].nome} foi aprovado(a) com nota ${listaDeAlunos[i].nota}`)
-        }
-    }
+    return listaDeAlunos.filter(function(aluno) {
+        return aluno.nota > 6;
+    })
 }
 
-foiAprovado()
+console.log(foiAprovado())
