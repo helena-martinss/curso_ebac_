6a77687eced1cd746386a1c0f060301a470e2650
Helena Martins <helena257424@gmail.com>
2025-06-03 20:42:30 -0300

    exercício finalizado

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-class Aluno {
-    constructor(nome, nota) {
-        this.nome = nome;
-        this.nota = nota;
-    }
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
 }
-
-const aluno1 = new Aluno('Helena', 9)
-const aluno2 = new Aluno('Maria', 5)
-const aluno3 = new Aluno('Júlia', 1)
-const aluno4 = new Aluno('Pedro', 7) 
-
-const listaDeAlunos = [aluno1, aluno2, aluno3, aluno4]
-
-function foiAprovado() {
-    return listaDeAlunos.filter(function(aluno) {
-        return aluno.nota > 6;
-    })
+console.log(multiplicar(3, 5));
+function saudacao(nome) {
+    return `Olá ` + nome;
 }
-
-console.log(foiAprovado())
+console.log(saudacao('Helena'));
---
+++ main.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b
+}
+
+console.log(multiplicar(3, 5))
+
+function saudacao(nome: string): string {
+    return `Olá ` + nome
+}
+
+console.log(saudacao('Helena'))
